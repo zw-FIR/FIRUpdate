b31b1e91fbea90bb3e021b255d60c23ec6b48ae0
zw-FIR <zw@fir.im>
2015-07-29 11:51:39 +0800

    更改fir

--- FIR.framework/Headers/FIR.h
+++ FIR.framework/Headers/FIR.h
@@ -18,6 +18,11 @@
  */
 +(void) handleCrashWithKey:(NSString*)crashKey;
 
+/* 手动上传crash,适用于用户try、catch未发生崩溃的情况
+ @param exception,捕获到的异常
+ */
++(void)sendCrashManually:(NSException *)exception;
+
 /* 设置自定义参数
  @param value NSString型value
  @param key   NSString型key
@@ -48,18 +53,19 @@
 +(void)checkForUpdateInAppStore:(FIResultBlock)callback;
 
 /** 检查FIR.im版本
- @param token FIR.im 上的用户Token,Token为空则根据appid搜索公测App信息
+ @param ApiToken FIR.im 上的用户ApiToken,Token为空则根据appid搜索公测App信息
  @param callback 回调
  @warning 不要用于线上版本
  
  回调时的2个参数: `NSDictionary *result` 和 `NSError *error`:
  result:
- "updateURL":更新地址
+ "installUrl":安装地址,使用的接口返回的install_url字段
+ "updateUrl":更新地址
  "version":版本号
  "changelog":更新日志
  "build":build版本号,
  */
-+(void)checkForUpdateInFIR:(FIResultBlock)callback token:(NSString *)token;
++(void)checkForUpdateInFIR:(FIResultBlock)callback apiToken:(NSString *)apiToken;
 
 @end
 
